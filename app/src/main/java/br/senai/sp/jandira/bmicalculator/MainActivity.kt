3072ee140865fa4d197d27ab30247d8e41a29743
Matheus Siqueira Silva <matheus.siqueira100@gmail.com>
2023-02-07 09:42:36 -0300

    dfd

---
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.senai.sp.jandira.bmicalculator
+
+import android.R
+import android.os.Bundle
+import android.view.textclassifier.SelectionEvent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmicalculator.ui.theme.BMICalculatorTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BMICalculatorTheme {
+
+}}}}
