556d38194a250ddd3ebb09c955ba588307b16b1e
Matheus Siqueira Silva <matheus.siqueira100@gmail.com>
2023-02-07 11:19:23 -0300

    teste

--- app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -1,10 +1,11 @@
 package br.senai.sp.jandira.bmicalculator
 
-import android.R
 import android.os.Bundle
 import android.view.textclassifier.SelectionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -14,6 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,5 +31,79 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BMICalculatorTheme {
+                Column() {
+                    CalculatorScreen();
 
-}}}}
+                }
+            }
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun CalculatorScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier.size(200.dp),
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription = "",
+                )
+                Text(
+                    text = stringResource(id = R.string.title),
+                    fontSize = 32.sp,
+                    color = Color.Blue,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 2.sp,
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxSize().
+                        padding(24.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.weight_label))
+                    OutlinedTextField(value = "'", onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp),
+                ) {
+                    Text(text = stringResource(id = R.string.height_label))
+                    OutlinedTextField(value = "'", onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.
+                            fillMaxWidth().
+                            padding(top=24.dp),
+                            shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.button_calculate), fontSize = 16.sp, modifier = Modifier.padding(12.dp))
+                }
+            }
+
+        }
+
+            Column() {
+            }
+        }
+        
+    }
